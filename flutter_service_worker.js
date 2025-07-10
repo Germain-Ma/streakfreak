4b65c63a7e51b0cbbb1de26a901b08cf42853cfd
Germain-Ma <germainmachleidt@gmail.com>
2025-07-10 16:40:41 -0500

    Built web
    Branch: main
    Commit: a32ae91c7b496297d367b1ad5a7016c8b75e416e
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "390ef1ec11c3400418167623e4d5e0a3",
+const RESOURCES = {"flutter_bootstrap.js": "f3502aa36cc2f5a52525e4e07b5b82dd",
 "version.json": "4bd3096da32f125b5f9a5a9eb6be448c",
 "index.html": "01ed575cfd435ae728aa80ef1ebfc2a2",
 "/": "01ed575cfd435ae728aa80ef1ebfc2a2",
-"main.dart.js": "8f5e8be200f0f41c1cd43c2e13821287",
+"main.dart.js": "7a2d3f32094f4d94308270df79af9bc5",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
