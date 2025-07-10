9242065b185787ba1ee8fb92326af455735aaba0
Germain-Ma <germainmachleidt@gmail.com>
2025-07-10 12:42:24 -0500

    Built web
    Branch: main
    Commit: efd8fac3111ea61ed56500b99859cb4865ee0fd0
    
    package:peanut 6.0.0

--- android/app/src/main/kotlin/com/example/streakfreak/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.streakfreak
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/models/activity.dart
+++
@@ -1,6 +0,0 @@
-class Activity {
-  final Map<String, String> fields;
-  Activity(this.fields);
- 
-  String? operator [](String key) => fields[key];
-} 
