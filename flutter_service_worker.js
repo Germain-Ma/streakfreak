3e09b53a8f2bd614ddc93b6121b982ba651737ae
Germain-Ma <germainmachleidt@gmail.com>
2025-07-12 14:46:11 -0500

    Built web
    Branch: main
    Commit: e520611f12a2f050d4ef454eb6ad922864fe4a69
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,18 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "45d7d0c7e04ada9fb686f32fa937358e",
+const RESOURCES = {"flutter_bootstrap.js": "54b8b0c1ed4a7c866f85795d4aec57b1",
 "version.json": "4bd3096da32f125b5f9a5a9eb6be448c",
-"index.html": "01ed575cfd435ae728aa80ef1ebfc2a2",
-"/": "01ed575cfd435ae728aa80ef1ebfc2a2",
+"index.html": "172c18431c91fe43081147200df62dfb",
+"/": "172c18431c91fe43081147200df62dfb",
 "main.dart.js": "10480b95b6d7aa15cb5de737e0b70f2b",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"manifest.json": "89a07a0d2a540d241af864eb530b00d8",
 "assets/AssetManifest.json": "1887b0854fdbf3d6e6224cb924db85b4",
 "assets/NOTICES": "0c21b4785b7b5637cde8e2508662ac18",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
