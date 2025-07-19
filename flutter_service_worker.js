b95f834d2311e79f66ede296cb6026ff6812d631
Germain-Ma <germainmachleidt@gmail.com>
2025-07-19 17:40:19 -0500

    Deploy latest version with GPS dots and layout fixes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3b2239b65f0e9273e8efbfdfb279f658",
+const RESOURCES = {"flutter_bootstrap.js": "bde0e37f5a54710633d3d43348e2b2e8",
 "version.json": "4bd3096da32f125b5f9a5a9eb6be448c",
 "index.html": "4f3926220f8e70afa1ff0b864794ec78",
 "/": "4f3926220f8e70afa1ff0b864794ec78",
-"main.dart.js": "7208aaa22c0b7287ddb86cf6be13d9f5",
+"main.dart.js": "5eeb25267c42ce1ce2649e37cc8a8e15",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
